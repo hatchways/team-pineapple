57b03560e2a2c1fb05c35b44e68d442950f8d98a
nicboy17 <33707120+nicboy17@users.noreply.github.com>
2019-08-01 16:15:06 -0400

    added linter for react

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,25 @@
-import React from "react";
-import { MuiThemeProvider } from "@material-ui/core";
-import { BrowserRouter, Route } from "react-router-dom";
+import React from 'react';
+import { MuiThemeProvider } from '@material-ui/core';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-import { theme } from "./themes/theme";
-import LandingPage from "./pages/Landing";
+import { theme } from './themes/theme';
+import LandingPage from './pages/Landing';
 
-import "./App.css";
+import './App.css';
 
-import PostDialog from './components/Dialog/PostDialog/PostDialog'
-import BoardDialog from "./components/Dialog/BoardDialog/BoardDialog";
+import PostDialog from './components/Dialog/PostDialog/PostDialog';
+import BoardDialog from './components/Dialog/BoardDialog/BoardDialog';
 
 function App() {
-  return (
-    <MuiThemeProvider theme={theme}>
-      <BrowserRouter>
-        <Route path = "/profile/:username/post/create" component = {PostDialog}/>
-        <Route path = "/profile/:username/board/create" component = {BoardDialog}/>
-        <Route path="/" component={LandingPage} />
-      </BrowserRouter>
-    </MuiThemeProvider>
-  );
+    return (
+        <MuiThemeProvider theme={theme}>
+            <BrowserRouter>
+                <Route path = "/profile/:username/post/create" component = {PostDialog}/>
+                <Route path = "/profile/:username/board/create" component = {BoardDialog}/>
+                <Route path="/" component={LandingPage} />
+            </BrowserRouter>
+        </MuiThemeProvider>
+    );
 }
 
 export default App;
