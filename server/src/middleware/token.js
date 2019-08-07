80ba56a754798aded0979b3388096090e7cc3b6c
kevncheng <37379325+kevncheng@users.noreply.github.com>
2019-08-06 21:07:09 -0400

    Changed middleware

--- server/src/middleware/token.js
+++ server/src/middleware/token.js
@@ -1,21 +1,22 @@
 const jwt = require('jsonwebtoken');
 const secret = 'secret';
 
-module.exports = function () {
-    return function (req, res, next) {
-        const token = req.body.token || req.body.query || req.headers['access-token']; // Check for token in body, URL, or headers
+module.exports = function(req, res, next) {
+    // Get token from header
+    const token = req.header('access-token');
 
-        if (token) {
-            jwt.verify(token, secret, function (err, decoded) {
-                if (err) {
-                    res.json({ success: false, message: 'Token invalid' });
-                } else {
-                    req.decoded = decoded;
-                    next();
-                }
-            });
-        } else {
-            res.json({ success: false, message: 'No token provided' });
-        }
-    };
+    // Check if not token
+    if (!token) {
+        return res.status(401).json({ msg: 'No token, authorization denied' });
+    }
+
+    // Verify token
+    try {
+        const decoded = jwt.verify(token, secret);
+
+        req.user = decoded.user;
+        next();
+    } catch (err) {
+        res.status(401).json({ msg: 'Token is not valid' });
+    }
 };
