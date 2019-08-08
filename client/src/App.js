8631e43be59417b4563b8440be0f1e9971bfd7c3
mrchrischan <chanchris1117@gmail.com>
2019-08-08 10:13:52 -0400

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -7,11 +7,10 @@ import { theme } from './themes/theme';
 import LogIn from './pages/LogIn.js';
 import SignUp from './pages/SignUp.js';
 import Profile from './pages/Profile.js';
-import NotFound from './pages/NotFound.js';
 
 import PostDialog from './components/Dialog/PostDialog/PostDialog';
 import BoardDialog from './components/Dialog/BoardDialog/BoardDialog';
-import InterestQuiz from './components/InterestQuiz.js'
+import PostPage from './pages/Post/PostPage';
 
 import './App.css';
 
@@ -19,13 +18,14 @@ function App() {
     return (
         <MuiThemeProvider theme={theme}>
             <BrowserRouter>
-                <Route exact path='/login' component={LogIn} />
-                <Route exact path='/signup' component={SignUp} />
+                <Switch>
+                    <Route exact path='/' component={LogIn} />
+                    <Route exact path='/signup' component={SignUp} />
+                </Switch>
                 <Route path='/profile/:username' component={Profile} />
                 <Route path='/profile/:username/post/create' component={PostDialog} />
                 <Route path='/profile/:username/board/create' component={BoardDialog} />
-                <Route path='/profile/:username/interestquiz' component={InterestQuiz} />
-                <Route component = {NotFound} />
+                <Route path='/post/:id' component={PostPage}/>
             </BrowserRouter>
         </MuiThemeProvider>
     );
