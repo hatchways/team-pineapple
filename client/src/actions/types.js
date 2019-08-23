c76c2f08dcb08c552366e6423f2d5593e135f299
kevncheng <37379325+kevncheng@users.noreply.github.com>
2019-08-23 14:15:48 -0400

    Redux Store for Sign up

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -2,9 +2,14 @@
 export const LOGIN = 'LOGIN';
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 export const LOGIN_ERROR = 'LOGIN_ERROR';
+export const LOGING_IN = 'LOGING_IN';
 export const LOGIN_RESPONSE = 'LOGIN_RESPONSE';
 export const EDIT_PROFILE_SUCCESS = 'EDIT_PROFILE_SUCCESS';
 export const EDIT_PROFILE_FAIL = 'EDIT_PROFILE_FAIL';
+
+export const AUTHORIZING = 'AUTHORIZING';
+export const SIGN_UP_SUCCESS = 'SIGN_UP_SUCCESS';
+export const SIGN_UP_FAIL = 'SIGN_UP_FAIL';
 
 export const LOGOUT = 'LOGOUT';
 export const LOGOUT_SUCCESS = 'LOGOUT_SUCCESS';
--- client/src/reducers/user.js
+++ client/src/reducers/user.js
@@ -4,30 +4,62 @@ import {
     LOGIN_RESPONSE,
     LOGIN_ERROR,
     GET_TOKEN_SUCCESS,
-    EDIT_PROFILE_SUCCESS
+    EDIT_PROFILE_SUCCESS,
+    LOGING_IN,
+    AUTHORIZING,
+    SIGN_UP_FAIL,
+    SIGN_UP_SUCCESS
 } from '../actions/types';
 
 const initialState = {
-    authenticated: false
+    authenticated: false,
+    loading: false,
+    error: { message: '', status: null }
 };
 
 export default (state = initialState, action) => {
     const response = action.response;
 
     switch (action.type) {
+    case LOGING_IN:
+    case AUTHORIZING:
+        return { ...state, loading: true };
     case LOGIN_SUCCESS:
-        return { authenticated: true, user: response.user, token: response.token };
+        return {
+            authenticated: true,
+            user: response.user,
+            token: response.token,
+            loading: false
+        };
     case LOGIN_ERROR:
-        return { ...state, authenticated: false, error: action.error };
+        return { ...state, authenticated: false, error: action.error, loading: false };
     case LOGIN_RESPONSE:
         delete state.error;
-        return { ...state };
+        return { ...state, loading: false };
     case LOGOUT_SUCCESS:
-        return { authenticated: false };
+        return initialState;
     case GET_TOKEN_SUCCESS:
-        return { ...state, authenticated: true, user: action.user, token: action.token };
+        return {
+            ...state,
+            authenticated: true,
+            user: action.user,
+            token: action.token,
+            loading: false
+        };
     case EDIT_PROFILE_SUCCESS:
-        return { ...state, user: { ...state.user, profile: action.payload.user.profile, name: action.payload.user.name } };
+        return {
+            ...state,
+            user: {
+                ...state.user,
+                profile: action.payload.user.profile,
+                name: action.payload.user.name
+            },
+            loading: false
+        };
+    case SIGN_UP_FAIL:
+        return { ...state, loading: false, error: action.payload };
+    case SIGN_UP_SUCCESS:
+        return { ...state, loading: false, error: action.payload };
     default:
         return state;
     }
